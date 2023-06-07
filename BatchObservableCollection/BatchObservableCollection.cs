a825a304a70419517a459935910afdeadbcbd110
Sebastian Born <vezixig@googlemail.com>
2023-06-07 12:36:20 +0200

    Initial Commit

---
+++ BatchObservableCollection/BatchObservableCollection.cs
@@ -0,0 +1,63 @@
+﻿namespace BatchObservableCollection
+{
+    using System.Collections.ObjectModel;
+    using System.Collections.Specialized;
+
+    /// <summary>Extension of <see cref="ObservableCollection{T}" /> with added functionality.</summary>
+    /// <typeparam name="T">The type of items in the collection.</typeparam>
+    public class BatchObservableCollection<T> : ObservableCollection<T>
+    {
+        #region Methods
+
+        /// <summary>Adds a range to the collection notifying only after all items have been added.</summary>
+        /// <param name="items">The items to add.</param>
+        public void AddRange(IList<T> items)
+        {
+            if (items == null) throw new ArgumentNullException(nameof(items));
+
+            var startIndex = Count;
+
+            foreach (var item in items) Items.Add(item);
+
+            OnCollectionChanged(new(NotifyCollectionChangedAction.Add, items, startIndex));
+            OnPropertyChanged(new(nameof(Count)));
+            OnPropertyChanged(new(nameof(Items)));
+        }
+
+        /// <summary>Removes all items from the collection that satisfy the specified condition.</summary>
+        /// <param name="evalFunc">A delegate that defines the condition to evaluate the items.</param>
+        public void RemoveAll(Func<T, bool> evalFunc)
+        {
+            var itemsToRemove = Items.Where(evalFunc).ToList();
+            itemsToRemove.ForEach(o => Items.Remove(o));
+
+            OnCollectionChanged(new(NotifyCollectionChangedAction.Remove, itemsToRemove));
+        }
+
+        /// <summary>Replaces all items of a collection with a new collection of replacement items.</summary>
+        /// <param name="replacement">The collection of replacement items.</param>
+        public void Replace(IList<T> replacement)
+        {
+            // Clear triggers NotifyCollectionChangedAction.Reset
+            Clear();
+            AddRange(replacement);
+        }
+
+        /// <summary>Replaces an item with a new one.</summary>
+        /// <param name="newElement">The new item.</param>
+        /// <param name="oldElement">The old item to be replace.</param>
+        /// <exception cref="ArgumentOutOfRangeException">If the provided old item is not part of the collection.</exception>
+        public void ReplaceElement(T newElement, T oldElement)
+        {
+            var index = Items.IndexOf(oldElement);
+            if (index == -1) throw new ArgumentOutOfRangeException(nameof(oldElement));
+
+            Items[index] = newElement;
+            OnCollectionChanged(
+                new(NotifyCollectionChangedAction.Replace, new List<T> { newElement }, new List<T> { oldElement })
+            );
+        }
+
+        #endregion
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using BatchObservableCollection;
+
+var myBatchCollection = new BatchObservableCollection<string>();
+myBatchCollection.CollectionChanged += (_, e) => Console.WriteLine($"Batch Collection changed ({e.Action}) - has now {myBatchCollection.Count} items.");
+
+var myCollection = new ObservableCollection<string>();
+myCollection.CollectionChanged += (_, e) => Console.WriteLine($"Observable Collection changed ({e.Action}) - has now {myCollection.Count} items.");
+
+// Add single items
+Console.WriteLine("Adding single Item");
+var val = Guid.NewGuid().ToString();
+myBatchCollection.Add(val);
+myCollection.Add(val);
+
+// Add range
+Console.WriteLine("\r\nAdding range");
+var batch = new List<string>();
+for (var i = 0; i < 10; i++) batch.Add(Guid.NewGuid().ToString());
+myBatchCollection.AddRange(batch);
+batch.ForEach(o => myCollection.Add(o));
+
+// Replace
+Console.WriteLine("\r\nReplacing all elements");
+myBatchCollection.Replace(new List<string> { Guid.NewGuid().ToString(), Guid.NewGuid().ToString() });
+
+// Replace Element
+Console.WriteLine("\r\nReplacing single element");
+myBatchCollection.ReplaceElement(Guid.NewGuid().ToString(), myBatchCollection.First());
+
+// Remove Elements
+Console.WriteLine("\r\nRemoving with evaluation function");
+myBatchCollection.RemoveAll(o => o.Contains('1'));
+
+Console.WriteLine("\r\nThis is the end. Press RETURN to exit.");
+Console.ReadLine();
